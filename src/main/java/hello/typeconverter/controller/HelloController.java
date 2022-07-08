f04589c581e5ee394c5a44b0d0f564fc8ea80095
shinyehwan <jghisjl007@naver.com>
2022-07-08 15:02:40 +0900

    스프링 타입 컨버터 - 스프링 포맷터 까지 끝!

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 public class HelloController {
 
     @GetMapping("/hello-v1")
-    public String helloV1(HttpServletRequest request){
+    public String helloV1(HttpServletRequest request) {
         String data = request.getParameter("data");
         Integer intValue = Integer.valueOf(data);
         System.out.println("intValue = " + intValue);
@@ -18,9 +19,15 @@ public class HelloController {
     }
 
     @GetMapping("/hello-v2")
-    public String helloV2(@RequestParam Integer data){
+    public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
         return "ok";
     }
 
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT= " + ipPort.getPort());
+        return "ok";
+    }
 }
